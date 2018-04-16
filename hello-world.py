9e7a22c0be409c72132b75e8321930319f21f9bb
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-04-16 16:34:31 +0200

    alters abfrage

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,7 @@
 # Hello World mit python
-print ("Hello World!")
+name = input("What's your name?")
+print ("Hello World!", name)
+
+alter = input("How old are you?")
+print("This year you are " , alter)
+print("Next year you are ", int (alter) + 1)
